b986133230284b2294f6668ab2eb409c45663949
Jon Arve Ovesen <jonovesen@gmail.com>
2025-03-08 20:50:04 +0100

    Update and rename Game to solver.h

---
+++ solver.h
@@ -0,0 +1,26 @@
+
+#ifndef SOLVER_H
+#define SOLVER_H
+
+#include <vector>
+#include <utility>
+
+class Solver {
+public:
+    Solver(int codeLength, int numColors);
+    std::vector<int> makeGuess();
+    void receiveFeedback(int correctPosition, int correctColor);
+    void reset();
+
+private:
+    int codeLength;
+    int numColors;
+    std::vector<std::vector<int>> possibleCodes;
+    std::vector<int> currentGuess;
+
+    void generateAllCodes();
+    int calculateFeedback(const std::vector<int>& guess, const std::vector<int>& code);
+    void prunePossibleCodes(int correctPosition, int correctColor);
+};
+
+#endif // SOLVER_H
